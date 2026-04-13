c06246929d232e47370b600113468fd28d17171e
ericvivancos <eric_vy@hotmail.com>
2026-04-14 00:13:00 +0200

    feat(pc-agent): initial WPF project setup

---
+++ pc-agent/Slumber.Agent.Windows/Slumber.Agent.Windows/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Slumber.Agent.Windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
