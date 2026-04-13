898c8cbcb6a82431a3cb8424d4a6d00e90f68e19
ericvivancos <eric_vy@hotmail.com>
2026-04-14 00:20:54 +0200

    feat(pc-agent): add media play/pause control

--- pc-agent/Slumber.Agent.Windows/Slumber.Agent.Windows/MainWindow.xaml.cs
+++ pc-agent/Slumber.Agent.Windows/Slumber.Agent.Windows/MainWindow.xaml.cs
@@ -1,24 +1,34 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Slumber.Agent.Windows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private const int KEYEVENTF_EXTENDEDKEY = 0x1;
+        private const int KEYEVENTF_KEYUP = 0x2;
+        private const byte VK_MEDIA_PLAY_PAUSE = 0xB3;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            TogglePlayPause();
+        }
+
+        private void TogglePlayPause()
+        {
+            // Key down
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0);
+
+            // Key up
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
         }
     }
 }
